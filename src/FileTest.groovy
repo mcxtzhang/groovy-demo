7dfe53c1ca973c5d3c42937a8d29713f61a275cb
zhangxutong <zhangxutong@imcoming.cn>
2017-06-05 18:51:03 +0800

    脚本变量作用域

---
+++ src/FileTest.groovy
@@ -0,0 +1,16 @@
+/**
+ * Intro: 
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/6/5.
+ * History:
+ */
+
+def srcFile = new File('list.groovy')
+/*srcFile.eachLine {
+    line-> println line
+}*/
+
+
+println srcFile.getBytes()
